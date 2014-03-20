252e927371e4e004716829a3a3eec87e9323f29a
jeffeld <eldridge.jeff@gmail.com>
2014-03-20 21:11:28 +0000

    Gallery updated

--- js/GalleryController.js
+++ js/GalleryController.js
@@ -3,42 +3,42 @@ function GalleryController ($scope) {
 
     $scope.Gallery = [
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/1.jpg',
             title : 'Candle holders',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/2.jpg',
             title : 'Glitter jars',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/3.jpg',
             title : 'Pen Pots',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/4.jpg',
             title : 'Lavender Bags',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/5.jpg',
             title : 'Candle holders',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/6.jpg',
             title : 'Glitter jars',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/7.jpg',
             title : 'Pen Pots',
             desc : 'Lorem ipsum...'
         },
         {
-            img : 'holder.js/140x140',
+            img : 'images/gallery/8.jpg',
             title : 'Lavender Bags',
             desc : 'Lorem ipsum...'
         }
