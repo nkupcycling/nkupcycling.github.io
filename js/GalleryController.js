95aae0c53fb120029019472a7215a4d3f5e1a2ab
Jeff <eldridge.jeff@gmail.com>
2014-03-21 12:31:54 +0000

    Updated imagery

--- js/GalleryController.js
+++ js/GalleryController.js
@@ -4,44 +4,55 @@ function GalleryController ($scope) {
     $scope.Gallery = [
         {
             img : 'images/gallery/1.jpg',
-            title : 'Candle holders',
-            desc : 'Lorem ipsum...'
+            title : 'Pink Handwriting',
+            desc : 'An attractive and useful pen pot'
         },
         {
             img : 'images/gallery/2.jpg',
-            title : 'Glitter jars',
-            desc : 'Lorem ipsum...'
+            title : 'Daisy',
+            desc : 'Can be used as a pen holder or flower pot'
         },
         {
             img : 'images/gallery/3.jpg',
-            title : 'Pen Pots',
-            desc : 'Lorem ipsum...'
+            title : 'Storage Jar',
+            desc : 'Decorated storage for all those little bits and pieces'
         },
-        {
-            img : 'images/gallery/4.jpg',
-            title : 'Lavender Bags',
-            desc : 'Lorem ipsum...'
-        },
+//        {
+//            img : 'images/gallery/4.jpg',
+//            title : 'Lavender Bags',
+//            desc : 'Lorem ipsum...'
+//        },
         {
             img : 'images/gallery/5.jpg',
-            title : 'Candle holders',
+            title : 'Flower',
             desc : 'Lorem ipsum...'
         },
         {
             img : 'images/gallery/6.jpg',
-            title : 'Glitter jars',
-            desc : 'Lorem ipsum...'
+            title : 'Purple Heart',
+            desc : 'A handy storage jar'
         },
         {
             img : 'images/gallery/7.jpg',
-            title : 'Pen Pots',
-            desc : 'Lorem ipsum...'
+            title : 'Coin Tray',
+            desc : 'Convenience for coins'
         },
         {
             img : 'images/gallery/8.jpg',
-            title : 'Lavender Bags',
-            desc : 'Lorem ipsum...'
+            title : 'Candle Holder',
+            desc : ''
+        },
+        {
+            img : 'images/gallery/9.jpg',
+            title : 'Material Flowers',
+            desc : 'Useful for paint brush storage'
+        },
+        {
+            img : 'images/gallery/10.jpg',
+            title : 'Large Candle Holder',
+            desc : 'Perfect for the dinner table'
         }
+
     ];
 
 }
